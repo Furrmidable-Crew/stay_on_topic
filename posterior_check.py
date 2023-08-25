0558c75a18124adfade7d902b8f12f1dd1275dde
Nicola <nicorb932@hotmail.com>
2023-08-25 22:10:10 +0200

    output parser in posterior check; commented prior check; doc preprocessing

--- posterior_check.py
+++ posterior_check.py
@@ -1,5 +1,10 @@
 import random
 
+from langchain.chains import LLMChain
+from langchain.prompts.prompt import PromptTemplate
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+
 from cat.mad_hatter.decorators import hook
 from cat.log import log
 
@@ -9,28 +14,65 @@ def before_cat_sends_message(message, cat):
     settings = cat.mad_hatter.plugins["stay_on_topic"].load_settings()
 
     if settings["posterior_check"]:
-        context = cat.mad_hatter.execute_hook("agent_prompt_declarative_memories", cat.working_memory["declarative_memories"])
 
-        prompt = f"""Rewrite the sentence in a JSON with this format:
-                            {{  
-                                'cheshire_cat': here the parts of the sentence related to the context, otherwise None
-                                'other': here the parts of the sentence not related to the context, otherwise None
-                            }}
-                        SENTENCE --> {message["content"]}
-                        CONTEXT --> {context}
-                    """
+        context = cat.mad_hatter.execute_hook("agent_prompt_declarative_memories",
+                                              cat.working_memory["declarative_memories"])
 
-        answer = cat.llm(prompt)
+        ccat_schema = ResponseSchema(name="support",
+                                     description="here the parts of the sentence related to the context, otherwise None")
+        other_schema = ResponseSchema(name="other",
+                                      description="here the parts of the sentence not related to the context, otherwise None")
+        schema = [ccat_schema, other_schema]
+        output_parser = StructuredOutputParser.from_response_schemas(schema)
 
-        log(f"POSTERIOR**************\n{answer}", "ERROR")
+        template = f"""
+        Support: a sentence related to asking for technical support about the Cheshire Cat AI framework. E.g. hooks, tools, the Rabbit Hole, The Mad Hatter, plugins and many other topics.
+        Other: a sentence that is asking something general, different from the previous topics.
+        
+        Given this context --> {{context}}
+        Rewrite this sentence --> {{text}}
 
-        answer = answer.replace("null", "None")
+        {{format_instructions}}"""
 
-        json_answer = eval(answer)
+        prompt = PromptTemplate(
+            template=template,
+            input_variables=["context", "text"],
+            output_parser=output_parser,
+            partial_variables={"format_instructions": output_parser.get_format_instructions()}
+        )
 
-        message["content"] = json_answer["cheshire_cat"]
+        chain = LLMChain(
+            llm=cat._llm,
+            prompt=prompt,
+            verbose=True
+        )
 
-        if json_answer["cheshire_cat"] == "None":
+        answer = chain.run({"context": context,
+                            "text": message["content"]})
+
+        output_dict = output_parser.parse(answer)
+        log(output_dict, "CRITICAL")
+
+        # prompt = f"""Rewrite the sentence in a JSON with this format:
+        #                     {{
+        #                         'cheshire_cat': here the parts of the sentence related to the context, otherwise None
+        #                         'other': here the parts of the sentence not related to the context, otherwise None
+        #                     }}
+        #                 SENTENCE --> {message["content"]}
+        #                 CONTEXT --> {context}
+        #             """
+        #
+        # answer = cat.llm(prompt)
+        #
+        # log(f"POSTERIOR**************\n{answer}", "ERROR")
+        #
+        # answer = answer.replace("null", "None")
+        #
+        # json_answer = eval(answer)
+        #
+        message["content"] = output_dict["support"]
+
+        if output_dict["support"] is None:
             message["content"] = random.choice([
                 "Sorry, I have no memories about that.",
                 "I can't help you on this topic.",
--- prior_check.py
+++ prior_check.py
@@ -1,6 +1,12 @@
 import random
 
+from langchain.chains import LLMChain
+from langchain.prompts.prompt import PromptTemplate
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import StructuredOutputParser
+
 from cat.mad_hatter.decorators import hook
+from cat.log import log
 
 
 @hook
@@ -21,24 +27,53 @@ def before_agent_starts(agent_input, cat):
             "output": out_of_topic_answer
         }
 
-    if settings["prior_check"]:
-        answer = cat.llm(f"""I'll give you a sentence, you should split it a JSON structure with this keys and values:
-                            {{  
-                                'cheshire_cat': the parts of the sentence related to the context, otherwise None
-                                'other': the parts of the sentence not related to the context, otherwise None
-                            }}
-                        SENTENCE --> {cat.working_memory["user_message_json"]["text"]}
-                        CONTEXT --> {agent_input["declarative_memory"]}
-                    """)
-        from cat.log import log
-        answer = answer.replace("null", "None")
-        json_answer = eval(answer)
-        log(answer, "ERROR")
-        if json_answer["cheshire_cat"] == "None":
-            return {
-                "output": out_of_topic_answer
-            }
-        agent_input["input"] = json_answer["cheshire_cat"]
+    # if settings["prior_check"]:
+    #
+    #     ccat_schema = ResponseSchema(name="cheshire_cat",
+    #                                  description="the question on the same topic of the context")
+    #     other_schema = ResponseSchema(name="other",
+    #                                   description="everything else")
+    #     schema = [ccat_schema, other_schema]
+    #     output_parser = StructuredOutputParser.from_response_schemas(schema)
+    #
+    #     template = f"""Given this context --> {{context}}
+    #     extract the part of the text that is on the same topic --> {{text}}
+    #
+    #     {{format_instructions}}"""
+    #
+    #     prompt = PromptTemplate(
+    #         template=template,
+    #         input_variables=["context", "text"],
+    #         output_parser=output_parser,
+    #         partial_variables={"format_instructions": output_parser.get_format_instructions()}
+    #     )
+    #
+    #     chain = LLMChain(
+    #         llm=cat._llm,
+    #         prompt=prompt,
+    #         verbose=True
+    #     )
+    #
+    #     answer = chain.run({"context": agent_input["declarative_memory"],
+    #                        "text": cat.working_memory["user_message_json"]["text"]})
+    #     log(answer, "CRITICAL")
+        # answer = cat.llm(f"""I'll give you a sentence, you should split it a JSON structure with this keys and values:
+        #                     {{
+        #                         'cheshire_cat': the parts of the sentence related to the context, otherwise None
+        #                         'other': the parts of the sentence not related to the context, otherwise None
+        #                     }}
+        #                 SENTENCE --> {cat.working_memory["user_message_json"]["text"]}
+        #                 CONTEXT --> {agent_input["declarative_memory"]}
+        #             """)
+        # from cat.log import log
+        # answer = answer.replace("null", "None")
+        # json_answer = eval(answer)
+        # log(answer, "ERROR")
+        # if json_answer["cheshire_cat"] == "None":
+        #     return {
+        #         "output": out_of_topic_answer
+        #     }
+        # agent_input["input"] = json_answer["cheshire_cat"]
 
         return None
 
